fbfd50b4d3605b59f50d3676a7e42eee4047e5a8
DenisseCardozaP <denisse_cardoza@hotmail.com>
2023-04-29 15:45:17 -0600

    Review procedures

--- database/procedures/review_procedures.sql
+++ database/procedures/review_procedures.sql
@@ -86,3 +86,106 @@ BEGIN
         course_id = courseId AND r.user_id = userId AND r.review_active=1;
 END;
 DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `sp_update_review`(IN `id` INT, IN `message` VARCHAR(255), IN `rate` INT, IN `active` BOOLEAN)
+BEGIN
+    UPDATE reviews
+    SET
+        review_message = IFNULL(message, review_message),
+        review_rate = IFNULL(rate, review_rate),
+        review_modified_at = NOW(),
+        review_active = IFNULL(active, review_active)
+    WHERE
+        review_id = id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE sp_get_course_reviews(IN course_id INT)
+BEGIN
+    SELECT
+        r.review_id AS `id`,
+        r.review_message AS `message`,
+        r.review_rate AS `rate`,
+        r.course_id AS `courseId`,
+        r.user_id AS `userId`,
+        r.review_created_at AS `createdAt`,
+        r.review_modified_at AS `modifiedAt`,
+        r.review_active AS `active`,
+        CONCAT(u.user_name, ' ', u.user_last_name) AS `userName`,
+        u.profile_picture AS `profilePicture`
+    FROM
+        reviews AS r
+    INNER JOIN
+        users AS u
+    ON
+        r.user_id = u.user_id
+    WHERE
+        r.course_id = course_id
+    ORDER BY
+        review_created_at DESC;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE sp_get_review_by_course_and_user(IN course_id INT, IN user_id INT)
+BEGIN
+    SELECT
+        r.review_id AS `id`,
+        r.review_message AS `message`,
+        r.review_rate AS `rate`,
+        r.course_id AS `courseId`,
+        r.user_id AS `userId`,
+        r.review_created_at AS `createdAt`,
+        r.review_modified_at AS `modifiedAt`,
+        r.review_active AS `active`,
+        CONCAT(u.user_name, ' ', u.user_last_name) AS `userName`,
+        u.profile_picture AS `profilePicture`
+    FROM
+        reviews AS r
+    INNER JOIN
+        users AS u
+    ON
+        r.user_id = u.user_id
+    WHERE
+        r.course_id = course_id AND r.user_id = user_id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE sp_get_review_by_id(IN id INT)
+BEGIN
+    SELECT
+        r.review_id AS `id`,
+        r.review_message AS `message`,
+        r.review_rate AS `rate`,
+        r.course_id AS `courseId`,
+        r.user_id AS `userId`,
+        r.review_created_at AS `createdAt`,
+        r.review_modified_at AS `modifiedAt`,
+        r.review_active AS `active`,
+        CONCAT(u.user_name, ' ', u.user_last_name) AS `userName`,
+        u.profile_picture AS `profilePicture`
+    FROM
+        reviews AS r
+    INNER JOIN
+        users AS u
+    ON
+        r.user_id = u.user_id
+    WHERE
+        r.review_id = id;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE sp_deactivate_review(IN id INT)
+BEGIN
+    UPDATE
+        `reviews`
+    SET
+        `review_active` = FALSE
+    WHERE
+        `review_id` = id;
+END$$
+DELIMITER ;
--- server/Repositories/ReviewRepository.php
+++ server/Repositories/ReviewRepository.php
@@ -14,82 +14,28 @@ class ReviewRepository implements ReviewRepositoryInterface {
             :course_id,
             :user_id
         )
-        /* INSERT INTO reviews(
-            review_message,
-            review_rate,
-            course_id,
-            user_id
-        )
-        SELECT
-            :message,
-            :rate,
-            :course_id,
-            :user_id
-        FROM
-            dual
-        WHERE
-            :message IS NOT NULL
-            AND :rate IS NOT NULL
-            AND :course_id IS NOT NULL
-            AND :user_id IS NOT NULL */
     SQL;
 
     private const UPDATE = <<<'SQL'
-        UPDATE
-            reviews
-        SET
-            review_message = IFNULL(:message, review_message),
-            review_rate = IFNULL(:rate, review_rate),
-            review_modified_at = NOW(),
-            review_active = IFNULL(:active, review_active)
-        WHERE
-            review_id = :id
+        CALL `sp_update_review`( 
+            :id, 
+            :message, 
+            :rate, 
+            :active
+        );
     SQL;
 
     private const FIND_ALL_BY_COURSE = <<<'SQL'
-        SELECT
-            r.review_id AS `id`,
-            r.review_message AS `message`,
-            r.review_rate AS `rate`,
-            r.course_id AS `courseId`,
-            r.user_id AS `userId`,
-            r.review_created_at AS `createdAt`,
-            r.review_modified_at AS `modifiedAt`,
-            r.review_active AS `active`,
-            CONCAT(u.user_name, ' ', u.user_last_name) AS `userName`,
-            u.profile_picture AS `profilePicture`
-        FROM
-            reviews AS r
-        INNER JOIN
-            users AS u
-        ON
-            r.user_id = u.user_id
-        WHERE
-            course_id = :course_id
-        ORDER BY
-            review_created_at DESC;
+        CALL `sp_get_course_reviews`(
+            :course_id
+        );
     SQL;
 
     private const FIND_ONE_BY_COURSE_AND_USER_ID = <<<'SQL'
-        SELECT
-            r.review_id AS `id`,
-            r.review_message AS `message`,
-            r.review_rate AS `rate`,
-            r.course_id AS `courseId`,
-            r.user_id AS `userId`,
-            r.review_created_at AS `createdAt`,
-            r.review_modified_at AS `modifiedAt`,
-            r.review_active AS `active`,
-            CONCAT(u.user_name, ' ', u.user_last_name) AS `userName`,
-            u.profile_picture AS `profilePicture`
-        FROM
-            reviews AS r
-        INNER JOIN
-            users AS u
-        ON
-            r.user_id = u.user_id
-        WHERE
-            course_id = :courseId AND r.user_id = :userId;
+        CALL `sp_get_review_by_course_and_user`(
+            :courseId, 
+            :userId
+        );
     SQL;
 
     private const FIND_BY_COURSE = <<<'SQL'
@@ -102,34 +48,15 @@ class ReviewRepository implements ReviewRepositoryInterface {
     SQL;
 
     private const FIND_ONE = <<<'SQL'
-        SELECT
-        r.review_id AS `id`,
-            r.review_message AS `message`,
-            r.review_rate AS `rate`,
-            r.course_id AS `courseId`,
-            r.user_id AS `userId`,
-            r.review_created_at AS `createdAt`,
-            r.review_modified_at AS `modifiedAt`,
-            r.review_active AS `active`,
-            CONCAT(u.user_name, ' ', u.user_last_name) AS `userName`,
-            u.profile_picture AS `profilePicture`
-        FROM
-            reviews AS r
-        INNER JOIN
-            users AS u
-        ON
-            r.user_id = u.user_id
-        WHERE
-        r.review_id  = :id
+        CALL `sp_get_review_by_id`(
+            :id
+        );
     SQL;
 
     private const DELETE = <<<'SQL'
-        UPDATE
-            `reviews`
-        SET
-            `review_active` = FALSE
-        WHERE
-            `review_id` = :id;
+        CALL `sp_deactivate_review`(
+            :id
+        );
     SQL;
     
     public function create(Review $review): int {
